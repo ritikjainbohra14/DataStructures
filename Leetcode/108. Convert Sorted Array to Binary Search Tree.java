675c0d7a8d9e4edc9cb1524918bcebfd4e0b0409
ritikjainbohra14 <91238196+ritikjainbohra14@users.noreply.github.com>
2023-05-23 01:12:52 +0530

    commited

--- Leetcode/108. Convert Sorted Array to Binary Search Tree.java
+++ Leetcode/108. Convert Sorted Array to Binary Search Tree.java
@@ -1,39 +1,38 @@
-import java.util.*;
 
 
 //Definition for a binary tree node.
-public class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
+// public class TreeNode {
+//       int val;
+//       TreeNode left;
+//       TreeNode right;
+//       TreeNode() {}
+//       TreeNode(int val) { this.val = val; }
+//       TreeNode(int val, TreeNode left, TreeNode right) {
+//           this.val = val;
+//           this.left = left;
+//           this.right = right;
+//       }
+//   }
  
-class Solution {
-    public TreeNode sortedArrayToBST(int[] nums) {
-        int l = 0;
-        int r = nums.length-1;
-        return CreateNode(nums, l, r);
-    }
+// class Solution {
+//     public TreeNode sortedArrayToBST(int[] nums) {
+//         int l = 0;
+//         int r = nums.length-1;
+//         return CreateNode(nums, l, r);
+//     }
 
-    private TreeNode CreateNode(int[] nums,int l,int r){
+//     private TreeNode CreateNode(int[] nums,int l,int r){
 
-        if(l>r){
-            return null;
-        }
+//         if(l>r){
+//             return null;
+//         }
 
-        int mid = (l+r)/2;
-        TreeNode root = new TreeNode(nums[mid]);
+//         int mid = (l+r)/2;
+//         TreeNode root = new TreeNode(nums[mid]);
 
-        root.left = CreateNode(nums, l, mid-1);
-        root.right = CreateNode(nums, mid+1, r);
+//         root.left = CreateNode(nums, l, mid-1);
+//         root.right = CreateNode(nums, mid+1, r);
 
-        return root;
-    }
-}
+//         return root;
+//     }
+// }
