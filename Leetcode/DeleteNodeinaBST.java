10cc58349787d7497ae8af03e6f677821322b5ca
ritikjainbohra14 <91238196+ritikjainbohra14@users.noreply.github.com>
2023-05-24 08:22:08 +0530

    commited

---
+++ Leetcode/DeleteNodeinaBST.java
@@ -0,0 +1,61 @@
+public class DeleteNodeinaBST {
+
+ //Definition for a binary tree node.
+ public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
+class Solution {
+
+    public TreeNode InorderSuccessors(TreeNode root){
+        while(root.left != null){
+            root = root.left;
+        }
+
+        return root;
+    }
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+
+        if(root == null){
+            return null;
+        }
+
+        if(root.val < key){
+            root.right = deleteNode(root.right, key);
+        }
+        else if(root.val > key){
+            root.left = deleteNode(root.left, key);
+        }else{
+
+            if(root.left == null && root.right == null){
+                return null;
+            }
+
+            if(root.left == null){
+                return root.right;
+            }else if(root.right == null){
+                 return root.left;
+            }
+
+            TreeNode IS = InorderSuccessors(root.right);
+            root.val = IS.val;
+            root.right = deleteNode(root.right, IS.val);
+            
+            
+            }
+
+  return root;   
+}
+
+}
+}
