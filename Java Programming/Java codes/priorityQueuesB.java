799345a0302c94f674e86a698cad606e49ae2277
ritikjainbohra14 <91238196+ritikjainbohra14@users.noreply.github.com>
2023-05-24 12:44:42 +0530

    commited

--- Java Programming/Java codes/priorityQueuesB.java
+++ Java Programming/Java codes/priorityQueuesB.java
@@ -16,6 +16,27 @@ public class priorityQueuesB {
             return this.rank - s2.rank;
         }
     }
+
+    static class Heap{
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        public void add(int data){
+
+            arr.add(data);
+
+            int x = arr.size()-1; // x is chlid
+            int par = (arr.size()-2)/2; //  par is parent
+
+            while (arr.get(x) < arr.get(par)) {
+               //swap
+               
+               int temp = arr.get(x);
+               arr.set(x, arr.get(par));
+               arr.set(par, temp);
+            }
+
+        }
+    }
     public static void main(String args[]){
         PriorityQueue<Student> pq = new PriorityQueue<>();
 
