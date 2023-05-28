3c7dad03ebb295ff477615835c18857c074c7fe5
ritikjainbohra14 <91238196+ritikjainbohra14@users.noreply.github.com>
2023-05-28 21:07:33 +0530

    commited

--- Java Programming/Java codes/Hashmap.java
+++ Java Programming/Java codes/Hashmap.java
@@ -110,21 +110,67 @@ public class Hashmap {
 
            //*****************Find Itinary from Tickets*******************//
 
-           HashMap<String, String> tickets = new HashMap<>();
+          //  HashMap<String, String> tickets = new HashMap<>();
 
-           tickets.put("Chennai", "Bengaluru");
-           tickets.put("Mumbai", "Delhi");
-           tickets.put("Goa", "Chennai");
-           tickets.put("Delhi", "Goa");
+          //  tickets.put("Chennai", "Bengaluru");
+          //  tickets.put("Mumbai", "Delhi");
+          //  tickets.put("Goa", "Chennai");
+          //  tickets.put("Delhi", "Goa");
 
 
 
-           String start = getStart(tickets);
-           System.out.print(start);
-           for (String keys : tickets.keySet()) {
-            System.out.print("-->" + tickets.get(start));
-            start = tickets.get(start);
-           }
+          //  String start = getStart(tickets);
+          //  System.out.print(start);
+          //  for (String keys : tickets.keySet()) {
+          //   System.out.print("-->" + tickets.get(start));
+          //   start = tickets.get(start);
+          //  }
+
+
+          //*********************Largest Subarray with zero sum******************//
+
+          // int arr[]= {15, -2, 2, -8, 1, 7, 10, 23};
+
+          HashMap<Integer, Integer> map = new HashMap<>();
+
+          // int sum = 0;
+          // int len = 0;
+          // int j = 0;
+
+          // for (int i = 0; i < arr.length; i++) {
+          //   sum += arr[i];
+          //   if (map.containsKey(sum)) {
+          //     len = Math.max(len, i-map.get(sum) );
+          //   }else{
+          //     map.put(sum, i);
+          //   }
+          // }
+
+          // System.out.println(len);
+          
+          //************************Subaaray sum equal to k***********************//
+
+          int arr[] = {10,2,-2,-20,10};
+          int ans = 0; 
+          int sum = 0;
+          int k = -10;
+          
+
+          //(sum, count)
+
+          map.put(0, 1);
+
+         for (int i = 0; i < arr.length; i++) {
+          sum += arr[i];
+          if(map.containsKey(sum-k)){
+            ans += map.get(sum-k);
+          }
+
+          map.put(sum, map.getOrDefault(sum, 0) + 1);
+         } 
+
+
+         System.out.println(ans);
         
     }    
 }
