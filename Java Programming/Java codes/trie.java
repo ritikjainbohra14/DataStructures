fbee9fe98159c46ae489d34d7868bdfba61d26fe
ritikjainbohra14 <91238196+ritikjainbohra14@users.noreply.github.com>
2023-05-31 23:27:22 +0530

    commited

--- Java Programming/Java codes/trie.java
+++ Java Programming/Java codes/trie.java
@@ -3,22 +3,28 @@ public class trie {
     static class Node{
         Node children[] = new Node[26];
         boolean eow = false;
+        // prefic Notation
+        int freq;
 
         Node(){
             for (int i = 0; i < 26; i++) {
                 children[i] = null;
             }
+
+            freq  = 1; // prefix Notation
         }
     }
 
     public static Node root = new Node();
 
-    public static void imsert(String word){
+    public static void insert(String word){
         Node curr = root;
         for (int level = 0; level < word.length(); level++) {
             int idx = word.charAt(level) - 'a';
             if(curr.children[idx] == null){
                 curr.children[idx] = new Node();
+            }else{
+                curr.children[idx].freq++; // prefix notation
             }
 
             curr = curr.children[idx];
@@ -42,14 +48,39 @@ public class trie {
 
     }
 
-    public static void main(String args[]){
-        String words[] = {"the", "a", "there", "their", "any", "thee"};
+    public static boolean wordBreak(String key){
 
-        for (int i = 0; i < words.length; i++) {
-            imsert(words[i]);
+        if(key.length() == 0){
+            return true;
+        }
+        for (int i = 1; i < key.length(); i++) {
+            //sunstring
+            if(search(key.substring(0, i)) && wordBreak(key.substring(i))){
+                return true;
+            }   
         }
 
-        System.out.println(search("thee"));
+        return false;
+    }
+
+    public static void main(String args[]){
+        // String arr[] = {"i", "like", "sam", "samsung", "mobile", "ice"};
+
+        // for (int i = 0; i < arr.length; i++) {
+        //     insert(arr[i]);
+        // }
+
+        // String key = "ilikesung";
+
+        // System.out.println(wordBreak(key));
+
+        String arr[] = {"zebra", "dog", "duck", "dove"};
+
+        for (int i = 0; i < arr.length; i++) {
+            insert(arr[i]);
+        }
+
+
 
     }
     
