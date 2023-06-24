fb412d7bdc2d83fc68938d3307e3bd882f1a300e
ritikjainbohra14 <91238196+ritikjainbohra14@users.noreply.github.com>
2023-06-24 23:23:04 +0530

    commited

--- Java Programming/Java codes/dynamicProgramming.java
+++ Java Programming/Java codes/dynamicProgramming.java
@@ -385,6 +385,39 @@ public class dynamicProgramming {
         return x+y;
     }
 
+    public static boolean wildCardMatching(String s1, String s2){
+        int n = s1.length();
+        int m = s2.length();
+
+        boolean dp[][] = new boolean[n+1][m+1];
+
+        dp[0][0] = true;
+
+        for (int i = 1; i < dp.length; i++) {
+            dp[i][0] = false;
+        }
+
+        for (int i = 1; i < dp[0].length; i++) {
+            if(s2.charAt(i-1) == '*' && dp[0][i-1]){
+                dp[0][i] = true;
+            }else{
+                dp[0][i] = false;
+            }
+        }
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if((s1.charAt(i-1) == s2.charAt(j-1)) || s2.charAt(j-1) == '*' || s2.charAt(j-1) == '?' ){
+                    dp[i][j] = dp[i][j-1];
+                }else{
+                    dp[i][j] = false;
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+
     public static void main(String args[]){
         // int n = 5;
         // int f[] = new int[n+1];
@@ -451,10 +484,15 @@ public class dynamicProgramming {
         // String word1 = "intention";
         // String word2 = "execution";
 
-        String word1 = "abc";
-        String word2 = "sbd";
+        // String word1 = "abc";
+        // String word2 = "sbd";
 
-        System.out.println(editDistance(word1, word2));
+        // System.out.println(editDistance(word1, word2));
+
+        String text = "baaabab";
+        String pattern = "****ba*****ab";
+
+        System.out.println(wildCardMatching(text, pattern));
     }
 
 }
